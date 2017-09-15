a6ebdb01041aa47193d90a31a710bed996408025
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-09-15 20:53:53 +0300

    Admin

--- api/recipes/admin.py
+++ api/recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
+
 
 # Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
--- api/recipes/models.py
+++ api/recipes/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
 class Ingredient(models.Model):
     name = models.CharField(max_length=256)
     amount = models.FloatField()
     unit = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
 
+
+@python_2_unicode_compatible
 class Recipe(models.Model):
     uuid = models.UUIDField()
     name = models.CharField(max_length=256)
     imagePath = models.URLField()
     description = models.TextField()
     ingredients = models.ManyToManyField(Ingredient, 'recipes')
+
+    def __str__(self):
+        return self.name
