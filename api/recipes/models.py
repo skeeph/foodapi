d70f750fce6b433af398f8b459e9b3aafa73ddef
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-09-22 08:38:11 +0300

    Change ingredients relation from ManyToMany to ForeignKey

--- api/recipes/models.py
+++ api/recipes/models.py
@@ -8,6 +8,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=256)
     amount = models.FloatField()
     unit = models.CharField(max_length=10)
+    recipe = models.ForeignKey("recipes.Recipe", on_delete=models.CASCADE, related_name="ingredients")
 
     def __str__(self):
         return self.name
@@ -20,7 +21,6 @@ class Recipe(models.Model):
     name = models.CharField(max_length=256)
     imagePath = models.URLField()
     description = models.TextField()
-    ingredients = models.ManyToManyField(Ingredient, 'recipes')
     publ = models.BooleanField(default=False)
 
     def __str__(self):
