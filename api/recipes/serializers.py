9d617ec76c4c69307c51006e85f8c5e86d9e06f8
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-09-22 13:41:18 +0300

    Update recipe in serializer

--- api/recipes/serializers.py
+++ api/recipes/serializers.py
@@ -18,25 +18,31 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('uuid', 'name', 'imagePath', 'description', 'ingredients', 'publ')
 
     def create(self, validated_data):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+
         ingredients = validated_data.pop('ingredients')
         uuid = validated_data['uuid']
         if Recipe.objects.filter(uuid=uuid).count() == 0:
-            recipe = Recipe.objects.create(**validated_data)
+            recipe = Recipe.objects.create(user=user, **validated_data)
             for ingredient in ingredients:
-                ing = Ingredient.objects.create(**ingredient)
-                recipe.ingredients.add(ing)
+                ing = Ingredient.objects.create(recipe=recipe, **ingredient)
             return recipe
         else:
             return Recipe.objects.first()
 
     def update(self, instance, validated_data):
+        for ing in instance.ingredients.all():
+            ing.delete()
         ingredients = validated_data.pop('ingredients')
         instance.name = validated_data.pop('name')
-        instance.id = validated_data.pop('uuid')
+        instance.uuid = validated_data.pop('uuid')
         instance.imagePath = validated_data.pop('imagePath')
         instance.description = validated_data.pop('description')
         instance.publ = validated_data.pop('publ')
+
         for ingredient in ingredients:
-            ing = Ingredient.objects.create(**ingredient)
-            # FIXME instance.ingredients.add(ing)
+            Ingredient.objects.create(recipe_id=instance.pk, **ingredient)
         return instance
